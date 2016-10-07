563f49ae8d0e1cf0d62329ecec05591fef37fa63
Danielyan86 <516495459@qq.com>
2016-10-07 22:02:50 +0800

    [update] update program

--- Homework/aDong/count_word_in_a_file_test.py
+++ Homework/aDong/count_word_in_a_file_test.py
@@ -4,19 +4,15 @@ from aDong import count_words_in_a_file as COUNT
 
 
 class MyTestCase(unittest.TestCase):
-    def Test_count_words(self):
+    def test_count_words(self):
         with open('test.txt', 'w+') as f:
             f.write("How to write a pythonic program\nI don't know")
         test_obj = COUNT.CountWord('test.txt')
         self.assertEqual(test_obj.count_words_sum(), 9)
 
+    def test_count_words_second(self):
         test_obj = COUNT.CountWord('test2.txt')
         self.assertTrue(isinstance(test_obj.count_words_sum(), int))
-
-        # self.assertEqual(True, False)
-
-
-
 
 
 if __name__ == '__main__':
--- Homework/aDong/count_words_in_a_file.py
+++ Homework/aDong/count_words_in_a_file.py
@@ -1,6 +1,5 @@
 # count the English words from a file
 import requests
-import json
 
 
 class CountWord(object):
@@ -9,12 +8,13 @@ class CountWord(object):
         self.iciba_url = "http://www.iciba.com/index.php?callback=jQuery18308505977065421555_1475485177483&a=getWordMean&c=search&list=1,2,3,4,5,8,9,10,12,13,14,15,18,21,22,24,3003,3004,3005&word={0}&_=1475485178353"
 
     def count_words_sum(self):
+        print("start to count the words in file: {0}".format(self.file_name))
         word_counter = 0
         with open(self.file_name, encoding="utf-8", mode='r') as f:
             for line in f.readlines():
                 word_counter = word_counter + len(self.check_word_spelling(line.split()))
 
-        print("{0}Sum number of this file:{1}{2}".format(10 * '=', word_counter, 10 * '='))
+        print("{0}Sum number of this file: {1}{2}".format(10 * '=', word_counter, 10 * '='))
         return word_counter
 
     def check_word_spelling(self, line):
@@ -22,19 +22,19 @@ class CountWord(object):
         return list((filter(self._call_iciba, line)))
 
     def _call_iciba(self, word):
+        headers = {"Content-Encoding": "gzip"}
         check_work_url = self.iciba_url.format(word)
-        resp = requests.get(url=check_work_url)
+        resp = requests.get(url=check_work_url, headers=headers)
         if resp.status_code == 200:
             if '"errmsg":"success"' in resp.text:
+                print(word)
                 return word
             else:
-                print("This is the wrong Englsih word{0}".format(word))
+                print("This is the wrong English word:=={0}==".format(word))
+        else:
+            raise AssertionError("Status code is not 200 from site iciba")
 
 
 if __name__ == '__main__':
     # o = CountWord()
-    list_a = range(20)
-    print(list_a)
-    b = filter(lambda x: x > 5 and x < 50, range(60))
-
-    print(list(b))
+    pass
